0d85e9295f40b3279cedb4bd7f4c51bafda95d2c
Gilson Gindre <gindrejunior@hotmail.com>
2021-09-25 13:09:45 -0300

    Post model

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # Slug is in admin page only for debug purposes
+    list_display = ('title', 'author', 'status', 'slug')
+    list_filter = ('status', 'published', 'author')
+    ordering = ('status',)
+    autocomplete_fields = ('author',)
+    search_fields = ('title', 'body', 'author')
+    date_hierarchy = 'published'
+    prepopulated_fields = {'slug': ('title',)}
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    author = models.ForeignKey(get_user_model(), related_name='posts', on_delete=models.CASCADE)
+    slug = models.SlugField(unique_for_date='published', max_length=250)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=10, default='draft')
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('-published',)
